19d95ba66a508df886dc2c12d720187a16bd05bc
wdavilaneto <wdavilaneto@gmail.com>
2013-08-15 01:23:13 -0300

    correcao da greet (teste ) task

--- guick-plugin/src/main/groovy/org/wdn/guick/GreetTask.groovy
+++ guick-plugin/src/main/groovy/org/wdn/guick/GreetTask.groovy
@@ -6,16 +6,9 @@ import org.gradle.api.tasks.TaskAction
 class GreetTask extends DefaultTask {
 
     def text
-    def destination
-
-    File getDestination() {
-        project.file(destination)
-    }
 
     @TaskAction
     def run() {
-        def file = getDestination()
-        file.parentFile.mkdirs()
-        file.write "Hello!"
+        println "Hello!"
     }
 }
--- guick-plugin/src/main/groovy/org/wdn/guick/GuickPlugin.groovy
+++ guick-plugin/src/main/groovy/org/wdn/guick/GuickPlugin.groovy
@@ -22,29 +22,37 @@ class GuickPlugin implements Plugin<Project> {
 
         try {
             String pack = conventions.basePackage
-            if (project.group != null) {
+            if (project?.group != null) {
                 pack = project.group.toString().toLowerCase()
             }
 
-            if (project.name != null) {
+            if (project?.name != null) {
                 pack = pack + "." + project.name?.replaceAll("-", "").replaceAll(" ", "").toLowerCase()
             }
 
-//            project.tasks.withType(GreetTask).whenTaskAdded { GreetTask testTask ->
-//                testTask.conventionMapping.map("basePackage", pack)
-//                testTask.conventionMapping.map("modelPackage", conventions.modelPackage)
-//                testTask.conventionMapping.map("modelPackage", conventions.servicePackage)
-//                testTask.conventionMapping.map("controllerPackage", conventions.controllerPackage)
-//                testTask.conventionMapping.map("views", conventions.views)
-//            }
+            project.tasks.withType(GreetTask).whenTaskAdded { GreetTask testTask ->
+//                testTask.conventionMapping.map("basePackage") {pack}
+//                testTask.conventionMapping.map("modelPackage") {conventions.modelPackage}
+//                testTask.conventionMapping.map("modelPackage") {conventions.servicePackage}
+//                testTask.conventionMapping.map("controllerPackage") {conventions.controllerPackage}
+//                testTask.conventionMapping.map("views") {conventions.views}
+//                testTask.conventionMapping.map('webAppClasspath') { project.tasks.getByName(WarPlugin.WAR_TASK_NAME).classpath }
+//                testTask.conventionMapping.map('webAppSourceDirectory') { getWarConvention(project).webAppDir }
+//                testTask.conventionMapping.map('classesDirectory') { project.sourceSets.main.output.classesDir }
+            }
 
         } catch (RuntimeException e) {
-            e.printStackTrace("Erro ao configurar Guick Plugin" , e)
+            e.printStackTrace("Erro ao configurar Guick Plugin", e)
         }
 
         GreetTask greetTask = project.tasks.create("greet", GreetTask)
         greetTask.text = "hellow world plugin"
         greetTask.description = 'Guick Plugin Test Task'
+        greetTask.group = WarPlugin.WEB_APP_GROUP
 
+    }
+
+    WarPluginConvention getWarConvention(Project project) {
+        project.convention.getPlugin(WarPluginConvention)
     }
 }
