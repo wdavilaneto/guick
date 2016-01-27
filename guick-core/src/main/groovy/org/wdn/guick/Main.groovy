0170a76ff061a2ad8b6e2b94dd5e86e4c74c8db0
wdavilaneto <qwer!01>
2016-01-27 11:34:41 -0200

    Repackage do gerado e  organização

--- guick-core/src/main/groovy/org/wdn/guick/Main.groovy
+++ guick-core/src/main/groovy/org/wdn/guick/Main.groovy
@@ -33,7 +33,8 @@ class Main {
         ExpandoMetaClass.disableGlobally()
         try {
             Main main = new Main();
-            main.project.initialize("../mprj-gsi-portfolio/")
+            //main.project.initialize("../mprj-gsi-portfolio/")
+            //main.project.initialize("../java/mprj-gsi-medicamentos")
 
             main.setTarget("stage/create-webapp")
             main.run()
