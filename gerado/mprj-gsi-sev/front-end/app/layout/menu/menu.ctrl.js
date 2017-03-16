01afc0cae7c92e5900c4aedcad096e61fdd01970
walter.davila <walter.davila@mprj.mp.br>
2017-03-16 13:51:51 -0300

    Acertos De Layout  e geraçao da opçao CARDS nos filters/lists

--- gerado/mprj-gsi-sev/front-end/app/layout/menu/menu.ctrl.js
+++ gerado/mprj-gsi-sev/front-end/app/layout/menu/menu.ctrl.js
@@ -1,20 +1,20 @@
-    (function() {
-        'use strict';
+(function() {
+    'use strict';
 
-        angular.module('sevApp').controller('MenuController', ['$rootScope', '$scope', '$http', '$interval', 'jsog',
-            function($rootScope, $scope, $http, $interval, jsog ) {
-                $scope.toothlet = function(){
-                    $(".content-header.content-header-processo, .left-side, .right-side, .left-bar, nav.navbar.navbar-default.navbar-fixed-top, .bt-group-controller, .panel-page-pdf-content, .mh-mid, .content, footer, .totalContentWrapper").toggleClass("stash");
-                    $(".content-document").toggleClass("stash-index");
-                };
-                $scope.openLogout = function(){
-                    $(".content-logout, .content-logout-page, footer nav.navbar").toggleClass("stash");
-                };
-                $scope.changeColection = function(){
-                    $(".card-collection, .table-collection, .bt-card-list").toggleClass("stash");
-                };
-                $scope.openContentExpand = function(){
-                    $(".content-expand, .mdi.mdi-chevron-down, .mdi.mdi-chevron-up").toggleClass("stash");
-                };
-        }]);
-    })();
+    angular.module('sevApp').controller('MenuController', ['$rootScope', '$scope', '$http', '$interval', 'jsog',
+        function($rootScope, $scope, $http, $interval, jsog ) {
+            $scope.toothlet = function(){
+                $(".content-header.content-header-processo, .left-side, .right-side, .left-bar, nav.navbar.navbar-default.navbar-fixed-top, .bt-group-controller, .panel-page-pdf-content, .mh-mid, .content, footer, .totalContentWrapper").toggleClass("stash");
+                $(".content-document").toggleClass("stash-index");
+            };
+            $scope.openLogout = function(){
+                $(".content-logout, .content-logout-page, footer nav.navbar").toggleClass("stash");
+            };
+            $scope.changeColection = function(){
+                $(".card-collection, .table-collection, .bt-card-list").toggleClass("stash");
+            };
+            $scope.openContentExpand = function(){
+                $(".content-expand, .mdi.mdi-chevron-down, .mdi.mdi-chevron-up").toggleClass("stash");
+            };
+    }]);
+})();
