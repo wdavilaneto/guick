228d2c16866391516ab061161fbf746cdaa92a82
gusmanzord <gustavocarvalhodg@gmail.com>
2017-03-28 09:47:54 -0300

    add paginação ao projeto / finalizado.

--- gerado/mprj-gsi-sev/front-end/app/layout/prototipo/pagination.js
+++ gerado/mprj-gsi-sev/front-end/app/layout/prototipo/pagination.js
@@ -7,15 +7,15 @@
 
     function PageController() {
         var vm = this;
- vm.totalItems = 64;
-  vm.currentPage = 4;
-  vm.setPage = function (pageNo) {
-    vm.currentPage = pageNo;
-  };
-  vm.maxSize = 5;
-  vm.maxSize = 5;
-  vm.bigTotalItems = 175;
-  vm.bigCurrentPage = 1;
-}
+        vm.totalItems = 64;
+        vm.currentPage = 4;
+        vm.setPage = function (pageNo) {
+            vm.currentPage = pageNo;
+        };
+        vm.maxSize = 5;
+        vm.maxSize = 5;
+        vm.bigTotalItems = 175;
+        vm.bigCurrentPage = 1;
+    }
 
 })();
