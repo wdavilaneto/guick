2fe710b6077abf02c4b2482482713881cd5eaa53
walter.davila <walter.davila@mprj.mp.br>
2019-10-04 17:28:44 -0300

    adding field to main list navigation

--- guick-core/src/main/resources/angular/webapp/src/app/material.module.ts
+++ guick-core/src/main/resources/angular/webapp/src/app/material.module.ts
@@ -40,6 +40,7 @@ import { MatTabsModule } from '@angular/material/tabs';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { MatTreeModule } from '@angular/material/tree';
+import { MdePopoverModule } from '@material-extended/mde';
 
 @NgModule({
   exports: [
@@ -85,6 +86,7 @@ import { MatTreeModule } from '@angular/material/tree';
     MatToolbarModule,
     MatTooltipModule,
     MatTreeModule,
+    MdePopoverModule,
   ],
 })
 export class MaterialModule {}
